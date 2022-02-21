860078892cdce63250dfd6a4e9d2521f0336c40a
J. Shiva pranav <95524862+ShivaPranavJ@users.noreply.github.com>
2022-02-21 23:15:47 +0530

    Add files via upload

---
+++ Bus.java
@@ -0,0 +1,40 @@
+package busResv;
+public class Bus {
+    private int busNo;
+    private boolean ac;
+    private int capacity;
+    Bus(int no, boolean ac, int cap)
+    {
+        this.busNo=no;
+        this.ac=ac;
+        this.capacity=cap;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+    public void setCapacity(int cap)
+    {
+        capacity=cap;
+    }
+
+    public boolean isAc() {
+        return ac;
+    }
+    public int getBusNo()
+    {
+        return busNo;
+    }
+    public void setAc(boolean val)
+    {
+        ac=val;
+    }
+    public void setBusNo(int cap)
+    {
+        capacity=cap;
+    }
+    public void displayBusInfo()
+    {
+        System.out.println("Bus No:"+ busNo+" Ac:"+ac+"Total Capacity: "+capacity);
+    }
+}
---
+++ BusDemo.java
@@ -0,0 +1,36 @@
+package busResv;
+import  java.util.*;
+public class BusDemo {
+    public static void main(String[] args) {
+        //Bus buses[]=new Bus[50]; // but ArrayList is better
+        ArrayList<Bus> buses=new ArrayList<Bus>();  // database is much more better than ArrayList
+        ArrayList<Booking> bookings = new ArrayList<Booking>();
+        buses.add(new Bus(1,true,45));
+        buses.add(new Bus(2,false,50));
+        buses.add(new Bus(3,true,48));
+
+        int userOpt=1;
+        Scanner sc = new Scanner(System.in);
+        for(Bus b:buses)
+        {
+           b.displayBusInfo();
+        }
+        while(userOpt==1) {
+            System.out.println("Enter 1 to Book and 2 to exit");
+            userOpt=sc.nextInt();
+            if(userOpt==1)
+            {
+                //System.out.println("Booking.....");
+                Booking booking=new Booking();
+                if(booking.isAvailable(bookings,buses))
+                {
+                    bookings.add(booking);
+                    System.out.println("Your booking is confirmed");
+                }
+                else
+                    System.out.println("Sorry, Bus is full. Try another bus or date. ");
+            }
+
+        }
+    }
+}
